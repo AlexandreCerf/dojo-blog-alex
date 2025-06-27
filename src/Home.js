231ab4eba01219eeb812d9b97c40727d7de5d278
Alexandre CERF <alexandrecerf.pro@gmail.com>
2025-06-27 13:56:53 +0200

    Some changes and new parts with list and props

--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,19 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
-    //let name = "Theo";
-
-    const [name, setName] = useState("Theo");
-    const [age, setAge] = useState(25);
-
-    const title = "Welcome to Alex blog";
-    const likes = 60;
-    const link = "https://github.com/AlexandreCerf/dojo-blog-alex";
-
-    const handleClick = () => {
-        console.log("Salut");
-    }
-    const handleClickAgain = (nameToFunction) => {
-        console.log("Salut " + nameToFunction);
-    }
-
-    const handleChangeName = () => {
-        setName("Vincent");
-        setAge(28)
-    }
-
+    const [blogs, setBlogs ] = useState([
+        {title: "New website", body:"lorem ipsum...", author:"Alex", id:1},
+        {title: "Welcome to my world !", body:"lorem ipsum...", author:"Alex", id:2},
+        {title: "Web dev tool tips", body:"lorem ipsum...", author:"Theo", id:3},
+        {title: "Custom UI for Apple", body:"lorem ipsum...", author:"Vincent", id:4}
+    ]);
 
 
     return ( 
         <div className="home">
-            <h2>{title}</h2>
-            <p>{ name } a { age } ans </p>
-            <button onClick={handleChangeName}>Change le nom et age</button>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={() => {handleClickAgain('Alex')}}>Click me again</button>
-            <p>Liked { likes } times</p>
-            <p>{ 10 }</p>
-            <p>{ "Hey" }</p>
-            <p>{ [1,2,3,4,5,6] }</p>
-            <p>{ Math.random()*10}</p>
-            <a href={link}> Repo github Project</a>
+            <BlogList blogs={blogs} title="All blogs"/>
         </div>
      );
 }
